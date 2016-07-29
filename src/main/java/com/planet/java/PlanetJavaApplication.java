a709cac94988b7a1c029dd3f43b91b3117779231
Bazlur Rahman Rokon <anm_brr@yahoo.com>
2016-07-30 03:13:40 +0600

    Add spring boot starter project structure

---
+++ src/main/java/com/planet/java/PlanetJavaApplication.java
@@ -0,0 +1,12 @@
+package com.planet.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanetJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanetJavaApplication.class, args);
+	}
+}
