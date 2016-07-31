66c494372959e320bcecbe82d40cbb9258b6739d
Bazlur Rahman <brahman@vantage.com>
2016-07-31 16:57:33 +0600

    Add a dummy layout for home page with bootstrap

--- src/main/java/net/planet/java/config/WebSecurityConfig.java
+++ src/main/java/net/planet/java/config/WebSecurityConfig.java
@@ -17,7 +17,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
 
         http.authorizeRequests()
-                .antMatchers("/", "home").permitAll()
+                .antMatchers("/", "home","/webjars/**", "/css/**", "/js/**", "/images/**").permitAll()
                 .anyRequest().authenticated();
     }
 }
